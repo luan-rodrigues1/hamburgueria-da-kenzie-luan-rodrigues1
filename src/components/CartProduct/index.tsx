4a64462906eeb95bfa2f7f9f36b5cf4d17ed998a
Luan rodrigues <rodrigues.luan20@gmail.com>
2023-01-03 16:41:27 -0300

    feat: counted for each product added

--- src/components/CartProduct/index.tsx
+++ src/components/CartProduct/index.tsx
@@ -1,14 +1,15 @@
-import { useContext } from "react"
+import { ReactNode, useContext } from "react"
 import { UserContext } from "../../contexts/UserContext"
 
 const CartProduct = ({infoProduct}: any) => {
 
-    const {deleteProductCart} = useContext(UserContext)
+    const {deleteProductCart, amountProduct} = useContext(UserContext)
     return <li>
         <img src={infoProduct.img} alt="" />
         <h2>{infoProduct.name}</h2>
         <span>{infoProduct.category}</span>
         <button onClick={() => deleteProductCart(infoProduct.id)}>Remover</button>
+        <h5>Qtd: {amountProduct(infoProduct.id) as ReactNode}</h5>
     </li>
 }
 
