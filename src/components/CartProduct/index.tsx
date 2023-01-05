e44e8937ac1f80025f7574d61c3769fec4aad4ab
Luan rodrigues <rodrigues.luan20@gmail.com>
2023-01-05 13:51:39 -0300

    fix: adjustment to component pattern

--- src/components/CartProduct/index.tsx
+++ src/components/CartProduct/index.tsx
@@ -5,14 +5,16 @@ const CartProduct = ({infoProduct}: any) => {
 
     const {deleteProductCart, amountProduct, addProductCart} = useContext(UserContext)
     
-    return <li>
-        <img src={infoProduct.img} alt="" />
-        <h2>{infoProduct.name}</h2>
-        <span>{infoProduct.category}</span>
-        <button onClick={() => deleteProductCart(infoProduct.id)}>-</button>
-        <h5>{amountProduct(infoProduct.id) as ReactNode}</h5>
-        <button onClick={() => addProductCart(infoProduct.id)}>+</button>
-    </li>
+    return (
+        <li>
+            <img src={infoProduct.img} alt="" />
+            <h2>{infoProduct.name}</h2>
+            <span>{infoProduct.category}</span>
+            <button onClick={() => deleteProductCart(infoProduct.id)}>-</button>
+            <h5>{amountProduct(infoProduct.id) as ReactNode}</h5>
+            <button onClick={() => addProductCart(infoProduct.id)}>+</button>
+        </li>
+    )
 }
 
 export default CartProduct
--- src/components/NoProducts/index.tsx
+++ src/components/NoProducts/index.tsx
@@ -6,11 +6,13 @@ const NoProducts = () => {
 
     const {searchValue, setSearchValue, setWorkingFilter} = useContext(UserContext)
 
-    return <div>
-        <img src={searchOff} alt="lupa desligada" />
-        <h2>Nenhum produto encontrado com a palavra "{searchValue}"</h2>
-        <button onClick={() => {return (setSearchValue(""), setWorkingFilter(false))}}>Limpe sua pesquisa e tente novamente</button>
-    </div>
+    return (
+        <div>
+            <img src={searchOff} alt="lupa desligada" />
+            <h2>Nenhum produto encontrado com a palavra "{searchValue}"</h2>
+            <button onClick={() => {return (setSearchValue(""), setWorkingFilter(false))}}>Limpe sua pesquisa e tente novamente</button>
+        </div>
+    )
 }
 
 export default NoProducts
